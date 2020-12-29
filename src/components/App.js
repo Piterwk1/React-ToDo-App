db1d348064a4a26ed7c43b2cb63101ae45a8c607
Piotr Kaminski <piterwk1@.wp.pl>
2020-12-29 13:31:22 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+import React, {Component} from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    <div className="App">
+      ToDo App cuda sie dzieja
+    </div>
+  );
+    }
+}
+
+export default App;
