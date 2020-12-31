b7c4e02f22ca9c7348d6639ab6c53e76464f426b
Piotr Kaminski <piterwk1@.wp.pl>
2020-12-31 13:54:10 +0100

    add activeTask and doneTask

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const Task = () => {
+const Task = (props) => {
+
+    const {text, date, id} = props.task
+
     return ( 
-        <h3>Task</h3>
+        <div>
+<p>
+    <strong>{text}</strong> - do <span>{date} </span>
+    <button onClick={() => props.change(id)}>Zostało zrobione</button>
+<button onClick={() => props.delete(id)}>X</button>
+</p>
+
+        </div>
      );
 }
  
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import Task from './Task'
 
-const TaskList = () => {
+const TaskList = (props) => {
+
+    const active = props.tasks.filter(task => task.active)
+    const done = props.tasks.filter(task => !task.active)
+    
+    
+const activeTasks = active.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change}/>)
+const doneTasks = done.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change}/>)
+
     return ( 
-        <div>
-            <h2>List Tasków</h2>
-            <Task/>
-            <Task/>
-            <Task/>
+        <>
+        <div className='active'>
+            <h1>Zadania do zrobienia</h1>
+            {activeTasks.length > 0 ? activeTasks : <p><strong>Nimasz zadnych zadan</strong></p>}
         </div>
+
+<hr/>
+
+        <div className='done'>
+    <h3>Zadania zrobione (<em>{done.length}</em>) </h3>
+    {done.length > 5 && <span style={{fontSize: 20}}>Wyswietlonych jest jedynie 5 ostatnich elementów</span>}
+            {doneTasks.slice(0, 5)}
+        </div>
+        </>
      );
 }
  
