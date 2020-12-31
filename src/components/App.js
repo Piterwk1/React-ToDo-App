9c04333d67e44980f6f19094482dd9af16b4df74
Piotr Kaminski <piterwk1@.wp.pl>
2020-12-31 14:22:00 +0100

    fixing date dispay in donetask

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ class App extends Component {
       { id: 1, text: "zrobić dobry uczynej", date: '2020-11-12', important: false, active: true, finishDate: null },
       { id: 2, text: "pomalować dom po sylwestrze", date: '2019-09-11', important: false, active: true, finishDate: null },
       { id: 3, text: "schudnąć 30 kilogramów", date: '2019-05-20', important: true, active: true, finishDate: null },
-      { id: 4, text: "sprzedać butelki po piwie (20 skrzynek)", date: '2020-11-12', important: false, active: false, finishDate: null },
+      { id: 4, text: "sprzedać butelki po piwie (20 skrzynek)", date: '2020-11-12', important: false, active: true, finishDate: null },
       { id: 5, text: "jeszcze raz pomalować dom", date: '2019-09-11', important: false, active: true, finishDate: null },
       { id: 6, text: "fryzjer!!!", date: '2019-05-20', important: true, active: true, finishDate: null },
       { id: 7, text: "nie odbierać poleconego od komornika", date: '2020-11-12', important: false, active: true, finishDate: null },
